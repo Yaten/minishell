cc69b00758d3ff9056c2cddd62fa3640ac7c5473
Rogerio Siqueira <rogerioprogramador2021@gmail.com>
2022-03-09 18:57:16 -0300

    prompt_concat adicionado

--- apps/main.c
+++ apps/main.c
@@ -6,7 +6,7 @@
 /*   By: rsiqueir <rsiqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:25:08 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/03/09 15:59:46 by rsiqueir         ###   ########.fr       */
+/*   Updated: 2022/03/09 18:54:51 by rsiqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,37 @@ char *find_path(char *abs_path)
 	return pointer;
 }
 
+char	*concat_and_clean(char *s1, char *s2)
+{
+	char	*str;
+
+	str = NULL;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, (ft_strlen(s1) + ft_strlen(s2) + 1));
+	free(s1);
+	return (str);
+}
+
+
 char *prompt_concat(t_prompt *prompt)
 {
-	char *right_path;
+	char 	*right_path;
+	char	*result;
 
 	right_path = find_path(prompt->path);
-	printf("%s@%s:~%s$ ", prompt->user, prompt->hostname, right_path);
+
+	result = ft_strdup(prompt->user);
+	result = concat_and_clean(result, "@");
+	result = concat_and_clean(result, prompt->hostname);
+	result = concat_and_clean(result, ":~");
+	result = concat_and_clean(result, right_path);
+	result = concat_and_clean(result, "$ ");
+	return (result);
 }
 
 
@@ -53,16 +78,17 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	char	**input;	
 	t_prompt prompt;
+	char *test;
 
 	fill_prompt(&prompt);
 
-	prompt_concat(&prompt);
-
+	test = prompt_concat(&prompt);
 	
+	printf("%s\n", test);
 	printf("%s\n", prompt.user);
 	printf("%s\n", prompt.hostname);
 	printf("%s\n", prompt.path);
 
-	// ft_event_loop(input);
+	free(test);
 	return (0);
 }
