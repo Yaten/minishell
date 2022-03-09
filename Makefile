585484870bd96f9045ad81e37b2593bad9528cf7
Willian-code <willianrosendodelima@outlook.com>
2022-03-09 11:39:32 -0300

    adicionando a estrutura do projeto

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME = ./bin/minishell
+DIRNAME = ./bin
+CC = clang -g
+CFLAGS = -Wall -Wextra -Werror
+APP = ./apps
+LIB_PATH = ./libft
+LIBFT = libft.a
+OBJDIR = ./objects
+SRCDIR_PARSE = ./sources/parse
+SRCDIR_RUNTIME = ./sources/run_time
+INCLUDE = ./includes
+REMOVE = rm -rf
+
+SRC_PARSE +=
+OBJPARSE = $(SRC_PARSE:.c=.o)
+OBJECTS_PARSE = $(addprefix $(OBJDIR)/, $(OBJPARSE))
+
+SRC_RUNTIME +=
+OBJRUNTIME = $(SRC_RUNTIME:.c=.o)
+OBJECTS_RUNTIME = $(addprefix $(OBJDIR)/, $(OBJRUNTIME))
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS_PARSE) $(OBJECTS_RUNTIME)
+	$(MAKE) -C $(LIB_PATH)
+	@mkdir -p ./bin/
+	$(CC) $(APP)/main.c -o $(NAME) $(OBJECTS_PARSE) $(OBJECTS_RUNTIME) $(LIB_PATH)/$(LIBFT)
+
+$(OBJDIR)/%.o: $(SRCDIR_PARSE)/%.c
+	@mkdir -p $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -I $(INCLUDE) -o $@
+
+$(OBJDIR)/%.o: $(SRCDIR_RUNTIME)/%.c
+	@mkdir -p $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -I $(INCLUDE) -o $@
+
+clean:
+	$(MAKE) fclean -C $(LIB_PATH)
+
+fclean: clean
+	$(REMOVE) $(OBJDIR)
+	$(REMOVE) $(DIRNAME)
+
+re: fclean all
+
+.PHONY: bonus clean fclean re
---
+++ includes/ft_minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minishell.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 09:27:22 by wrosendo          #+#    #+#             */
+/*   Updated: 2022/03/09 09:53:09 by wrosendo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+
+# define TRUE 1
+# define FALSE 0
+
+void	ft_event_loop(char *input);
+
+# endif
