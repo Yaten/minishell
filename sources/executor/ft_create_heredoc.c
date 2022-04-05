d81b0497541158d8a42d042ec02d6231a788427f
Yaten <prafael.ramalho@gmail.com>
2022-04-05 11:31:20 -0300

    heredoc working now Co-authored-by: Willian Lima @willianlim

--- sources/executor/ft_create_heredoc.c
+++ sources/executor/ft_create_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_create_heredoc.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:51:30 by prafael-          #+#    #+#             */
-/*   Updated: 2022/04/05 09:29:17 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/04/05 11:14:22 by prafael-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 
 void	ft_create_heredoc(char *line)
 {
+	int		i;
 	char	*s;
 	char	**tmp;
 
+	i = -1;
 	unlink("here_doc.txt");
 	tmp = ft_split(line, '<');
+	while (tmp[++i])
+		;
 	ft_list_add_last(g_data.cmd_table, ft_strtrim(tmp[0], " "));
 	g_data.cmd_table->end->fd_in = open("here_doc.txt", O_WRONLY | O_CREAT , 0777);
 	while (1)
 	{
 		ft_putstr_fd("heredoc> ", 1);
 		s = get_next_line(0);
-		if (!ft_strncmp(s, ft_strtrim(tmp[1], " "), \
-		ft_strlen(ft_strtrim(tmp[1], " "))))
+		if (!ft_strncmp(s, ft_strtrim(tmp[i - 1], " "), \
+		ft_strlen(ft_strtrim(tmp[i - 1], " "))))
 		{
 			free(s);
 			close(g_data.cmd_table->end->fd_in);
