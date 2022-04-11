68ef564f45d1a804646695776d52c985b05a450b
Willian-code <willianrosendodelima@outlook.com>
2022-04-11 16:42:29 -0300

    removing segfault from heredoc

--- sources/executor/ft_create_heredoc.c
+++ sources/executor/ft_create_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_create_heredoc.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:51:30 by prafael-          #+#    #+#             */
-/*   Updated: 2022/04/07 19:27:18 by prafael-         ###   ########.fr       */
+/*   Updated: 2022/04/08 09:05:21 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_create_heredoc(char *line)
 	int		i;
 	char	*s;
 	char	**tmp;
+	pid_t	pid;
 
 	i = -1;
 	unlink("here_doc.txt");
@@ -24,24 +25,53 @@ void	ft_create_heredoc(char *line)
 	while (tmp[++i])
 		;
 	ft_list_add_last(g_data.cmd_table, ft_strtrim(tmp[0], " "));
-	g_data.cmd_table->end->fd_in = open("here_doc.txt", O_WRONLY | O_CREAT , 0777);
-	signal(SIGINT, here_doc_signal);
+	g_data.cmd_table->end->fd_in = open("here_doc.txt", O_WRONLY | O_CREAT | O_TRUNC , 0777);
+	// signal(SIGINT, here_doc_signal);
 	g_data.here_bol = 1;
-	while (g_data.here_bol)
+	pid = fork();
+	if (pid == -1)
+		perror("fork ");
+	if (!pid)
 	{
-		ft_putstr_fd("heredoc> ", 1);
-		s = get_next_line(0);
-		if (!ft_strncmp(s, ft_strtrim(tmp[i - 1], " "), \
-		ft_strlen(ft_strtrim(tmp[i - 1], " "))))
+		signal(SIGINT, here_doc_signal);
+		// signal(SIGINT, SIG_DFL);
+		while (g_data.here_bol)
 		{
+			ft_putstr_fd("heredoc> ", 1);
+			// s = readline("heredoc> ");
+			s = get_next_line(0);
+			if (!ft_strncmp(s, ft_strtrim(tmp[i - 1], " "), \
+			ft_strlen(ft_strtrim(tmp[i - 1], " "))))
+			{
+				free(s);
+				close(g_data.cmd_table->end->fd_in);
+				exit(1);
+			}
+			ft_putendl_fd(s, g_data.cmd_table->end->fd_in);
+			// ft_strlcat(aux, s, ft_strlen(aux) + ft_strlen(s) + 1);
 			free(s);
-			close(g_data.cmd_table->end->fd_in);
-			break ;
 		}
-		ft_putstr_fd(s, g_data.cmd_table->end->fd_in);
-		// ft_strlcat(aux, s, ft_strlen(aux) + ft_strlen(s) + 1);
-		free(s);
 	}
-	g_data.cmd_table->end->fd_in = open("here_doc.txt", O_RDONLY | O_CREAT, 0777);
-	g_data.cmd_table->end->relation = ft_strdup("<<");
+	else
+	{
+		int	status;
+		// int i;
+
+		status = 0;
+		wait(&status);
+		// waitpid(pid, NULL, 0);
+		// printf("status: %d\n", status);
+		// return ;
+		// i = WEXITSTATUS(status);
+		// printf("retorno %d\n", i);
+		if (WEXITSTATUS(status) == 1)
+		{
+			close(g_data.cmd_table->end->fd_in);
+			g_data.cmd_table->end->fd_in = open("here_doc.txt", O_RDONLY | O_CREAT, 0777);
+			g_data.cmd_table->end->relation = ft_strdup("<<");
+		}
+		// close(close(g_data.cmd_table->end->fd_in);
+		// g_data.cmd_table->end->fd_in = open("here_doc.txt", O_RDONLY | O_CREAT, 0777);
+		// g_data.cmd_table->end->relation = ft_strdup("<<");
+	}
 }
--- sources/run_time/ft_signals.c
+++ sources/run_time/ft_signals.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_signals.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 13:47:58 by prafael-          #+#    #+#             */
-/*   Updated: 2022/04/07 19:19:00 by prafael-         ###   ########.fr       */
+/*   Updated: 2022/04/07 21:42:27 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_minishell.h"
 
 t_prompt prompt;
+extern t_shell g_data;
 
 /* Ctrl C */
 void sigint_handler(int sig)
@@ -27,8 +28,13 @@ void here_doc_signal(int sig)
 {
 	(void)sig;
 	// write(1, "\n", 1);
-	g_data.here_bol = 0;
-	ft_putstr_fd("\n", 1);
+	ft_insert(g_data.array, "?", "130");
+	// puts("");
+	exit (130);
+	// g_data.here_bol = 0;
+	// ft_putstr_fd("\n", 0);
+	// close(0);
+	// puts("");
 	//exit(130);
 }
 
