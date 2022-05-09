ad86ac8173d825d07d991bc68591287a0a396d67
Willian-code <willianrosendodelima@outlook.com>
2022-05-09 20:56:05 -0300

    removing free from g_data.operators variable

--- sources/run_time/ft_event_loop.c
+++ sources/run_time/ft_event_loop.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:46:25 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/09 16:34:30 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/09 20:07:31 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,8 @@
 
 static int	ft_clean_program(t_prompt *prompt)
 {
-	int		i;
-
-	i = -1;
 	ft_printf("\n");
 	ft_destroy_hash(g_data.array);
-	while (g_data.operators[++i])
-		free(g_data.operators[i]);
-	free(g_data.operators);
 	(void)prompt;
 	exit(0);
 }
@@ -53,28 +47,20 @@ void	ft_event_loop(t_prompt *prompt)
 {
 	t_doubly	*token;
 
-	/* token = ft_list_create(); */
 	while (TRUE)
 	{
-		/*
-		 * t_doubly	*token;
-		*/
 		ft_prompt_concat(prompt);
 		if (ft_print_dir(prompt) || ft_set_new_line(prompt))
 			continue ;
 		g_data.aux = prompt->input_string;
 		ft_expand(g_data.aux, 0);
 		ft_quoting(prompt, g_data.aux, 0);
-        token = ft_list_create();
+		token = ft_list_create();
 		if (!ft_tokenize(prompt, token))
-    		continue ;
+			continue ;
 		g_data.cmd_table = ft_list_create();
 		ft_parse(token);
-		/* ft_list_destroy(&g_data.cmd_table); */
-		/* ft_list_destroy(&token); */
-		/*
-			* free(token);
-			* token = NULL;
-			*/
+		// ft_list_destroy(&token);
+		// ft_list_destroy(&g_data.cmd_table);
 	}
 }
