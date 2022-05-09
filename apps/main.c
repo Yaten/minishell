d98d0920872d12f73f8a02b308039b5500998266
Willian-code <willianrosendodelima@outlook.com>
2022-05-09 20:45:16 -0300

    remove function 'init_operators'

--- apps/main.c
+++ apps/main.c
@@ -3,28 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prafael- <prafael-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:25:08 by prafael-          #+#    #+#             */
-/*   Updated: 2022/04/18 16:48:21 by prafael-         ###   ########.fr       */
+/*   Updated: 2022/05/09 20:44:34 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/ft_minishell.h"
 # include <errno.h>
 
-t_prompt prompt;
-t_shell	g_data;
-
-static void	init_operators(void)
-{
-	g_data.operators = (char **)ft_calloc(6, sizeof(char *));
-	g_data.operators[0] = ft_strdup("|");
-	g_data.operators[1] = ft_strdup(">");
-	g_data.operators[2] = ft_strdup("<");
-	g_data.operators[3] = ft_strdup(">>");
-	g_data.operators[4] = ft_strdup("<<");
-}
+t_prompt	prompt;
+t_shell		g_data;
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -32,7 +22,6 @@ int	main(int argc, char *argv[], char *envp[])
 	g_data.envp = envp;
 	g_data.array = ft_create_table(envp);
 	ft_init_signals();
-	init_operators();
 	ft_event_loop(&prompt);
 	return (0);
 }
