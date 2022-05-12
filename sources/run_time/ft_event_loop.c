b739addc9a53dea2d39eb0da127a9f98b337bdff
Willian-code <willianrosendodelima@outlook.com>
2022-05-11 23:24:21 -0300

    function parameter changed

--- sources/run_time/ft_event_loop.c
+++ sources/run_time/ft_event_loop.c
@@ -6,32 +6,32 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:46:25 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/09 20:07:31 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:49:01 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_minishell.h"
+#include "ft_minishell.h"
 
-static int	ft_clean_program(t_prompt *prompt)
+static int	ft_clean_program()
 {
-	ft_printf("\n");
+	ft_putstr_fd("\n", STDOUT_FILENO);
 	ft_destroy_hash(g_data.array);
-	(void)prompt;
+	// (void)prompt;
 	exit(0);
 }
 
-int	ft_print_dir(t_prompt *prompt)
+int	ft_print_dir()
 {
 	char	*buf;
 
-	buf = readline(prompt->result);
-	free(prompt->result);
+	buf = readline("MINISHELL> ");
+	// free(prompt->result);
 	if (!buf)
-		ft_clean_program(prompt);
+		ft_clean_program();
 	if (*buf)
 	{
 		add_history(buf);
-		ft_strlcpy(prompt->input_string, buf, MAXINPUT);
+		ft_strlcpy(g_data.input_string, buf, MAXINPUT);
 		free(buf);
 		buf = NULL;
 		return (0);
@@ -43,23 +43,23 @@ int	ft_print_dir(t_prompt *prompt)
 	}
 }
 
-void	ft_event_loop(t_prompt *prompt)
+void	ft_event_loop()
 {
-	t_doubly	*token;
+	// t_doubly	*token;
 
 	while (TRUE)
 	{
-		ft_prompt_concat(prompt);
-		if (ft_print_dir(prompt) || ft_set_new_line(prompt))
+		// ft_prompt_concat(prompt);
+		if (ft_print_dir() || ft_set_new_line())
 			continue ;
-		g_data.aux = prompt->input_string;
+		g_data.aux = g_data.input_string;
 		ft_expand(g_data.aux, 0);
-		ft_quoting(prompt, g_data.aux, 0);
-		token = ft_list_create();
-		if (!ft_tokenize(prompt, token))
+		ft_quoting(0);
+		g_data.token = ft_list_create();
+		if (!ft_tokenize())
 			continue ;
 		g_data.cmd_table = ft_list_create();
-		ft_parse(token);
+		ft_parse();
 		// ft_list_destroy(&token);
 		// ft_list_destroy(&g_data.cmd_table);
 	}
