64267a916cab75e900bc91e7b5f763d00131784c
Willian-code <willianrosendodelima@outlook.com>
2022-05-11 23:03:23 -0300

    code indentation changed

--- sources/hash_table/ft_destroy_hash.c
+++ sources/hash_table/ft_destroy_hash.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 09:38:55 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/03/23 13:02:37 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:45:47 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 void	ft_destroy_hash(t_hash *array)
 {
-	t_hash	*temp;
-	t_hnode	*aux;
 	int		i;
-    int     index;
+	int		index;
+	t_hnode	*aux;
+	t_hash	*temp;
 
 	i = -1;
 	temp = array;
 	index = ft_hashcode("?");
 	if (ft_find_key((t_hnode *)array[index].head, "?") != -1)
-	{
-        free (array[index].head->value);
-    }
+		free (array[index].head->value);
 	while (++i < TABLE_SIZE)
 	{
 		while (temp[i].head)
 		{
-            if (temp[i].head->key != NULL)
-            {
-                if (ft_strcmp(temp[i].head->key, "?"))
-        			free(temp[i].head->value);
-	    		free(temp[i].head->key);
-            }
+			if (temp[i].head->key != NULL)
+			{
+				if (ft_strcmp(temp[i].head->key, "?"))
+					free(temp[i].head->value);
+				free(temp[i].head->key);
+			}
 			aux = temp[i].head;
 			temp[i].head = temp[i].head->next;
 			free(aux);
