20c96a3ef5e6d7d807fa2c53e4b1ce4f9af462ad
Willian-code <willianrosendodelima@outlook.com>
2022-05-11 23:23:06 -0300

    function parameter changed

--- sources/parse/ft_tokenize.c
+++ sources/parse/ft_tokenize.c
@@ -6,17 +6,17 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 10:11:53 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/04 19:00:54 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 19:01:39 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
-int	ft_define_token(t_doubly *token)
+int	ft_define_token()
 {
 	t_node	*tmp;
 
-	tmp = token->begin;
+	tmp = g_data.token->begin;
 	while (tmp)
 	{
 		if (!ft_strcmp(tmp->val[0], "|"))
@@ -35,7 +35,7 @@ int	ft_define_token(t_doubly *token)
 			tmp->operators = "word";
 		tmp = tmp->next;
 	}
-	return (ft_syntax(token));
+	return (ft_syntax());
 }
 
 int	ft_is_space(int c)
@@ -55,7 +55,7 @@ static char	*ft_verify_token(char *end)
 	return (end);
 }
 
-int	ft_tokenize(t_prompt *prompt, t_doubly *token)
+int	ft_tokenize()
 {
 	char	*end;
 	char	*tmp;
@@ -65,8 +65,8 @@ int	ft_tokenize(t_prompt *prompt, t_doubly *token)
 
 	tmp = NULL;
 	input_string_size = 0;
-	begin = prompt->input_string;
-	input_string_size = ft_strlen(prompt->input_string);
+	begin = g_data.input_string;
+	input_string_size = ft_strlen(g_data.input_string);
 	while (*begin)
 	{
 		amount_blanks = 0;
@@ -84,16 +84,17 @@ int	ft_tokenize(t_prompt *prompt, t_doubly *token)
 			else
 				end++;
 		}
-		tmp =  ft_substr(prompt->input_string, begin - prompt->input_string, end - begin);
+		tmp = ft_substr(g_data.input_string, begin - g_data.input_string, \
+		end - begin);
 		if (input_string_size == amount_blanks)
 			return (0);
-		ft_list_add_last(token,tmp);
-		if (!ft_strcmp(token->end->val[0], "\0"))
-			token->end->val[0] = "space";
+		ft_list_add_last(g_data.token, tmp);
+		if (!ft_strcmp(g_data.token->end->val[0], "\0"))
+			g_data.token->end->val[0] = "space";
 		free(tmp);
 		begin = end;
 		while (ft_is_space(*begin))
 			begin++;
 	}
-	return (ft_define_token(token));
+	return (ft_define_token());
 }
