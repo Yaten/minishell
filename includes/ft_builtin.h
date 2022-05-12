01ff37b0f734663f349a56b26d7b77d37889dad9
Willian-code <willianrosendodelima@outlook.com>
2022-05-11 22:39:13 -0300

    removing the comments

--- includes/ft_builtin.h
+++ includes/ft_builtin.h
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 17:43:47 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/09 22:33:53 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 22:19:14 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,15 @@
 
 # include "ft_doubly_linked_list.h"
 
-/**
- * @brief
- *
- * @param tmp
- */
 void	ft_builtin(t_node *tmp);
-
-/**
- * @brief
- *
- * @return int
- */
 int		ft_builtin_path(char *aux);
-
-/**
- * @brief
- *
- * @param node
- * @return int
- */
 int		ft_builtin_check(char *aux);
-
-/**
- * @brief
- *
- * @return int
- */
 int		ft_pwd(void);
-
-/**
- * @brief
- *
- * @return int
- */
 void	ft_env(void);
-
 void	ft_unset(void);
-
 void	ft_export(t_node *tmp);
-
 int		ft_exit(void);
-
 void	ft_echo(t_node *tmp);
-
 void	ft_cd(void);
 
 #endif
