d650f0ccd547936e4b90eb8311be49826151784a
Willian-code <willianrosendodelima@outlook.com>
2022-05-11 22:42:21 -0300

    removing variable t_slist *list and headers stdio.h and ft_simple_linked_list.h

--- includes/ft_doubly_linked_list.h
+++ includes/ft_doubly_linked_list.h
@@ -6,16 +6,14 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 16:43:35 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/09 22:37:53 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 22:20:45 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_DOUBLY_LINKED_LIST_H
 # define FT_DOUBLY_LINKED_LIST_H
 
-# include <stdio.h>
 # include <stdlib.h>
-# include "ft_simple_linked_list.h"
 
 typedef struct s_node
 {
@@ -27,7 +25,6 @@ typedef struct s_node
 	int				operator_output;
 	char			*path;
 	char			**val;
-	t_slist			*list;
 	struct s_node	*prev;
 	struct s_node	*next;
 }t_node;
@@ -40,19 +37,12 @@ typedef struct s_doubly
 }t_doubly;
 
 t_node		*ft_node_create(char *val);
-
 t_doubly	*ft_list_create(void);
-
 void		ft_list_destroy(t_doubly **l_ref);
-
 void		ft_list_add_first(t_doubly *l, char *val);
-
 int			ft_list_is_empty(t_doubly *l);
-
 void		ft_list_print(const t_doubly *l);
-
 void		ft_list_print2(const t_doubly *l);
-
 void		ft_list_add_last(t_doubly *l, char *val);
 
 #endif
