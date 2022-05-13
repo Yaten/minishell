2b2ff76af2d7d2760cc5534e2a82e94c91032dc1
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:11:45 -0300

    changing the indentation

--- includes/ft_parse.h
+++ includes/ft_parse.h
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 14:21:32 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/11 22:10:04 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 23:42:50 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 # include "ft_doubly_linked_list.h"
 
 void	ft_parse(void);
-int		ft_find_path(char *aux);
-void	ft_create_cmd(t_node *begin, t_node *end);
-void	ft_destroy_list(void);
-void	ft_expand(char *input_string, int bool_quotes);
-void	ft_quoting(int bool_quotes);
-int		ft_tokenize(void);
 int		ft_syntax(void);
+int		ft_tokenize(void);
+void	ft_destroy_list(void);
+int		ft_find_path(char *aux);
+void	ft_quoting(int bool_quotes);
+void	ft_create_cmd(t_node *begin, t_node *end);
 t_node	*ft_open_files(t_node *begin, t_node *node);
+void	ft_expand(char *input_string, int bool_quotes);
 
 #endif
