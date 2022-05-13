56ab6cfd2f8e62d31b88e5f3b13b71f6c8720e51
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:06:12 -0300

    g_data.boll_paths variable removed

--- apps/main.c
+++ apps/main.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:25:08 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/11 18:23:33 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:20:34 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ t_shell	g_data;
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	g_data.boll_paths = 1;
-	g_data.envp = envp;
 	g_data.array = ft_create_table(envp);
+	g_data.envp = envp;
 	ft_init_signals();
 	ft_event_loop();
 	return (0);
