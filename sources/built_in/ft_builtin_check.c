e6098746e2d4cb931a28d217f2189314305bf255
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:14:10 -0300

    function parameter changed and now this function checks all builtins

--- sources/built_in/ft_builtin_check.c
+++ sources/built_in/ft_builtin_check.c
@@ -6,23 +6,27 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 15:41:44 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/10 15:30:56 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:41:11 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
-int	ft_builtin_check(char *aux)
+int	ft_builtin_check()
 {
-	if (aux == NULL)
-		return (g_data.cmd_table->end->is_builtin = FALSE);
-	if (!(ft_strncmp(aux, "cd", 2)))
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "env")))
+		return (g_data.cmd_table->end->is_builtin = 1);
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "pwd")))
+		return (g_data.cmd_table->end->is_builtin = 2);
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "echo")))
+		return (g_data.cmd_table->end->is_builtin = 3);
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "cd")))
 		return (g_data.cmd_table->end->is_builtin = 4);
-	if (!(ft_strncmp(aux, "exit", 4)))
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "exit")))
 		return (g_data.cmd_table->end->is_builtin = 5);
-	if (!(ft_strncmp(aux, "unset", 5)))
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "unset")))
 		return (g_data.cmd_table->end->is_builtin = 6);
-	if (!(ft_strncmp(aux, "export", 6)))
+	if (!(ft_strcmp(g_data.cmd_table->end->val[0], "export")))
 		return (g_data.cmd_table->end->is_builtin = 7);
 	return (g_data.cmd_table->end->is_builtin = FALSE);
 }
