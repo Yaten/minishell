d63b94119ccec1f9e211ecb0e8f6d91a8a1a4e2a
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:20:03 -0300

    removing unnecessary comments

--- sources/executor/ft_dup_in.c
+++ sources/executor/ft_dup_in.c
@@ -6,18 +6,16 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 11:31:48 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/05/10 15:47:57 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:56:19 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
-void	ft_dup_in(t_node *tmp, int *fd, int *fd_aux)
+void	ft_dup_in(t_node *tmp, int *fd_aux)
 {
 	if (tmp->operator_input == 1)
 		dup2(tmp->fd_in, STDIN_FILENO);
 	else
 		dup2(fd_aux[0], STDIN_FILENO);
-	(void)fd;
-	(void)tmp;
 }
--- sources/executor/ft_dup_out.c
+++ sources/executor/ft_dup_out.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 11:32:15 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/05/10 15:48:13 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:55:45 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,4 @@ void	ft_dup_out(t_node *tmp, int *fd)
 		dup2(tmp->fd_out, STDOUT_FILENO);
 	else if (g_data.pipe_count)
 		dup2(fd[1], STDOUT_FILENO);
-	(void)fd;
-	(void)tmp;
 }
