aa697f660ecf6346097ce8b228ef02eedfb2f94e
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:18:51 -0300

    just moving the 'i' around and setting the 'aux' variable NULL

--- sources/doubly_linked_list/ft_list_destroy.c
+++ sources/doubly_linked_list/ft_list_destroy.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:26:39 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/11 22:18:12 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 15:08:33 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 
 void	ft_list_destroy(t_doubly **l_ref)
 {
+	int			i;
 	t_doubly	*l;
 	t_node		*p;
 	t_node		*aux;
-	int			i;
 
+	aux = NULL;
 	l = *l_ref;
 	p = l->begin;
 	while (p != NULL)
 	{
+		i = 0;
 		aux = p;
 		p = p->next;
-		i = 0;
 		while (aux->val[i] != NULL)
 			free(aux->val[i++]);
 		free(aux->val);
 		free(aux->path);
-		free (aux);
+		free(aux);
 	}
 	free(l);
 	*l_ref = NULL;
