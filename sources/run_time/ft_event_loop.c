b902b2c5393bcf49b84a97efdd833f3ce42aac92
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:35:37 -0300

    putting the ft_executor function, removed unnecessary comments, modified readline input

--- sources/run_time/ft_event_loop.c
+++ sources/run_time/ft_event_loop.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 09:46:25 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/11 20:49:01 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/12 17:37:14 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@ static int	ft_clean_program()
 {
 	ft_putstr_fd("\n", STDOUT_FILENO);
 	ft_destroy_hash(g_data.array);
-	// (void)prompt;
-	exit(0);
+	exit(FALSE);
 }
 
 int	ft_print_dir()
 {
 	char	*buf;
 
-	buf = readline("MINISHELL> ");
-	// free(prompt->result);
+	buf = readline(RED"MINISHELL> "RESET);
 	if (!buf)
 		ft_clean_program();
 	if (*buf)
@@ -34,33 +32,27 @@ int	ft_print_dir()
 		ft_strlcpy(g_data.input_string, buf, MAXINPUT);
 		free(buf);
 		buf = NULL;
-		return (0);
+		return (FALSE);
 	}
 	else
 	{
 		free(buf);
-		return (1);
+		return (TRUE);
 	}
 }
 
 void	ft_event_loop()
 {
-	// t_doubly	*token;
-
 	while (TRUE)
 	{
-		// ft_prompt_concat(prompt);
 		if (ft_print_dir() || ft_set_new_line())
 			continue ;
 		g_data.aux = g_data.input_string;
-		ft_expand(g_data.aux, 0);
-		ft_quoting(0);
-		g_data.token = ft_list_create();
+		ft_expand(g_data.aux, FALSE);
+		ft_quoting(FALSE);
 		if (!ft_tokenize())
 			continue ;
-		g_data.cmd_table = ft_list_create();
 		ft_parse();
-		// ft_list_destroy(&token);
-		// ft_list_destroy(&g_data.cmd_table);
+		ft_exececutor();
 	}
 }
