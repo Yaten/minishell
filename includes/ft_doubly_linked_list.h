d6c3508d6ce21fef08e547096f7c2d41bceb994a
Willian-code <willianrosendodelima@outlook.com>
2022-05-12 21:09:39 -0300

    changing the indentation

--- includes/ft_doubly_linked_list.h
+++ includes/ft_doubly_linked_list.h
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 16:43:35 by prafael-          #+#    #+#             */
-/*   Updated: 2022/05/11 22:20:45 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/11 23:40:08 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,13 @@ typedef struct s_doubly
 	size_t	size;
 }t_doubly;
 
-t_node		*ft_node_create(char *val);
 t_doubly	*ft_list_create(void);
-void		ft_list_destroy(t_doubly **l_ref);
-void		ft_list_add_first(t_doubly *l, char *val);
+t_node		*ft_node_create(char *val);
 int			ft_list_is_empty(t_doubly *l);
 void		ft_list_print(const t_doubly *l);
+void		ft_list_destroy(t_doubly **l_ref);
 void		ft_list_print2(const t_doubly *l);
 void		ft_list_add_last(t_doubly *l, char *val);
+void		ft_list_add_first(t_doubly *l, char *val);
 
 #endif
