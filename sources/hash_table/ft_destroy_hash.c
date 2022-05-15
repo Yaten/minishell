579c21436665e04b1cb9424d5fb34b9d5d330b57
Willian-code <willianrosendodelima@outlook.com>
2022-05-15 16:12:09 -0300

    simplifying the code, removing the check for the '?'

--- sources/hash_table/ft_destroy_hash.c
+++ sources/hash_table/ft_destroy_hash.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 09:38:55 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/05/10 15:45:47 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/05/15 14:22:32 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,18 @@
 void	ft_destroy_hash(t_hash *array)
 {
 	int		i;
-	int		index;
 	t_hnode	*aux;
 	t_hash	*temp;
 
 	i = -1;
 	temp = array;
-	index = ft_hashcode("?");
-	if (ft_find_key((t_hnode *)array[index].head, "?") != -1)
-		free (array[index].head->value);
 	while (++i < TABLE_SIZE)
 	{
 		while (temp[i].head)
 		{
 			if (temp[i].head->key != NULL)
 			{
-				if (ft_strcmp(temp[i].head->key, "?"))
-					free(temp[i].head->value);
+				free(temp[i].head->value);
 				free(temp[i].head->key);
 			}
 			aux = temp[i].head;
